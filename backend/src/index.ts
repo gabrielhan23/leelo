1354339b5580327ff3e38b6073a6b188c0cea8ee
maxmwang <orangemax888@gmail.com>
2023-10-28 13:16:01 -0700

    backend: more boilerplate

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,23 @@
 import http from 'http';
 
 import * as dotenv from 'dotenv';
-import express from 'express'
+import express from 'express';
+import { Server } from 'socket.io';
+
+import connectDB from './config/db';
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 8080;
 
+connectDB();
+
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
+const io = new Server(server);
 
-server.listen(PORT, () => console.log('Server started on port ${PORT}'))
+io.on('connection', async (socket) => {
+  console.log('a user connected');
+});
 
-module.exports = app;
-
+server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
