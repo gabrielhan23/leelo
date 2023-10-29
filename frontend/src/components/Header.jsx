1e401942d53fe99085eb6feafe637c66e34ab54f
Ethan Yao <ethanyao128@gmail.com>
2023-10-28 18:56:55 -0700

    send help

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
         <nav className="navbar">
             <div className="header">
                 <Link to="/" className="header-logo"><img src={leet} className="logo-style"/></Link>
-                <Link to="/login" className="login-logo"><img src={login} className="login"/></Link>
+                <Link to="/login" className="login-logo"><img src={login} className="login-icon"/></Link>
                 <Link className="darktoggle-logo" onClick={() => console.log("switch to dark")}><img src={sun} className="darktoggle"/></Link>
             </div>
         </nav>
