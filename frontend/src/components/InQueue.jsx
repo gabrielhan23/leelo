c27d214f9544a873cc1c3c32ff02065437850901
Ethan Yao <ethanyao128@gmail.com>
2023-10-28 22:49:29 -0700

    small change #40 before the big commit

--- frontend/src/components/InQueue.jsx
+++ frontend/src/components/InQueue.jsx
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {
     Pressable,
 } from 'react-native';
-import { Link } from 'react-router-dom';
+import { Link, redirect } from 'react-router-dom';
 import '../App.css';
 import PlayerCounter from './PlayerCounter';
 
@@ -20,12 +20,19 @@ function Queue() {
         }
         return () => clearInterval(intervalId);
       }, [isRunning, time]);
-
-    if (playerCount == 4) {
+    
+    if (playerCount == 5) {
+        updatePlayerCount(6);
+        window.location.href = "game";
+    }
+    if (playerCount >= 4) {
+        if (playerCount == 4) {
+            updatePlayerCount(5); //temp just to get the timer to start running while still rendering
+        }
         return (
             //this should not be a link; change eventually to a div or the col/row thing, and have buttons in the div
             //the onMouseOver is temp, you can change it later
-            <Link to="/game" onMouseOver={()=>setTime(time + 1)}><div className="in-queue">{Math.floor(time / 60) < 10 ? "0" : ""}{Math.floor(time / 60)}:{time % 60 < 10 ? "0" : ""}{time % 60}</div></Link>
+            <div className="in-queue"><div className="gamefound">game found, loading...</div><div className="player-counter"><PlayerCounter players={playerCount} /></div></div>
         );
     } else {
         return ( 
